61ded00b44e67886ccf5a1a72b8e0bdfe72f22b4
patels95 <patels326@gmail.com>
2015-04-18 22:36:34 -0400

    connected to parse for users

---
+++ app/src/main/java/com/patels95/sanam/ribbit/RibbitApplication.java
@@ -0,0 +1,20 @@
+package com.patels95.sanam.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Sanam on 4/18/15.
+ */
+
+public class RibbitApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "dz54YtAhucArleX3cqpbyDw2YrsfaDe56xsbMwvk", "m7cle3APfeVnGvV4LjMPth33xyz6dF7FSNvxpBLD");
+    }
+}
--- app/src/main/java/com/patels95/sanam/ribbit/SignUpActivity.java
+++ app/src/main/java/com/patels95/sanam/ribbit/SignUpActivity.java
@@ -1,19 +1,53 @@
 package com.patels95.sanam.ribbit;
 
+import android.app.AlertDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 public class SignUpActivity extends ActionBarActivity {
+
+    @InjectView(R.id.usernameField) EditText mUsername;
+    @InjectView(R.id.passwordField) EditText mPassword;
+    @InjectView(R.id.emailField) EditText mEmail;
+    @InjectView(R.id.signupButton) Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
+        ButterKnife.inject(this);
 
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // get String text from user input. trim() will remove whitespace
+                String username = mUsername.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+
+                if(username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.signup_error_message)
+                        .setTitle(R.string.signup_error_title)
+                        .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else {
+                    // create the new user
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
