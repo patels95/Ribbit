841c0fdac5d12071e3b48127197628a229591e75
patels95 <patels326@gmail.com>
2015-07-19 12:59:48 -0400

    receiving messages and message item layout

--- app/src/main/java/com/patels95/sanam/ribbit/model/ParseConstants.java
+++ app/src/main/java/com/patels95/sanam/ribbit/model/ParseConstants.java
@@ -1,19 +1,23 @@
 package com.patels95.sanam.ribbit.model;
 
 public final class ParseConstants {
-    //Class name
+    // CLASS NAMES
     public static final String CLASS_MESSAGES = "Messages";
 
-    //Field names
+    // FIELD NAMES
+    // user
     public static final String KEY_USERNAME = "username";
     public static final String KEY_FRIENDS_RELATION = "friendsRelation";
 
+    // message
     public static final String KEY_RECIPIENT_IDS = "recipientIds";
     public static final String KEY_SENDER_ID = "senderId";
     public static final String KEY_SENDER_NAME = "senderName";
     public static final String KEY_FILE = "file";
     public static final String KEY_FILE_TYPE = "fileType";
+    public static final String KEY_CREATED_AT = "createdAt";
 
+    // file
     public static final String TYPE_IMAGE = "image";
     public static final String TYPE_VIDEO = "video";
 }
--- app/src/main/java/com/patels95/sanam/ribbit/view/InboxFragment.java
+++ app/src/main/java/com/patels95/sanam/ribbit/view/InboxFragment.java
@@ -2,18 +2,66 @@ package com.patels95.sanam.ribbit.view;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 import com.patels95.sanam.ribbit.R;
+import com.patels95.sanam.ribbit.model.ParseConstants;
+
+import java.util.List;
 
 public class InboxFragment extends ListFragment {
+
+    public static final String TAG = InboxFragment.class.getSimpleName();
+
+    protected List<ParseObject> mMessages;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
-        return rootView;
+        return inflater.inflate(R.layout.fragment_inbox, container, false);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseObject> query = new ParseQuery<>(ParseConstants.CLASS_MESSAGES);
+        query.whereEqualTo(ParseConstants.KEY_RECIPIENT_IDS, ParseUser.getCurrentUser().getObjectId());
+        query.addDescendingOrder(ParseConstants.KEY_CREATED_AT);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> messages, ParseException e) {
+                getActivity().setProgressBarIndeterminateVisibility(false);
+                if (e == null) {
+                    // we found messages
+                    mMessages = messages;
+
+                    String[] usernames = new String[mMessages.size()];
+                    int i = 0;
+                    for (ParseObject message : mMessages) {
+                        usernames[i] = message.getString(ParseConstants.KEY_SENDER_NAME);
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                            getListView().getContext(),
+                            android.R.layout.simple_list_item_1,
+                            usernames);
+                    setListAdapter(adapter);
+                }
+                else {
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
     }
 }
